d58494d92f324dc9e74e65a74873dd2fa2affdcd
whizline <whizlinerogers@gmail.com>
2021-06-24 10:23:42 +0100

    task

---
+++ site.php
@@ -0,0 +1,10 @@
+<!Doctype html>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+
+</body>
+</html>
---
+++ task.php
@@ -0,0 +1,49 @@
+
+
+
+<!DOCTYPE html>
+
+<html>
+
+<body>
+Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end. 
+
+
+<br>
+
+
+<?php
+
+echo "<br>";
+$num  = range(0,5);
+echo "First Method" ."<br>";
+
+print_r ($num);
+
+echo "<br>";
+echo "<br>";
+
+echo "Second Method" ."<br>";
+
+var_dump ($num);
+
+echo "<br>";
+
+?>
+
+<br>Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
+<br>
+<?php
+
+echo "<br>";
+
+$a=array("a"=>2,"b"=>4,"c"=>15);
+
+echo array_sum($a);
+
+?>
+
+
+</body>
+
+</html>
