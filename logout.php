43751af72ab49f05791d76913129bb969239cdf5
whizline <whizlinerogers@gmail.com>
2021-07-04 15:07:42 +0100

    logout updates

--- logout.php
+++ logout.php
@@ -2,10 +2,6 @@
 
 session_start(); //to make sure its same session
 
-//to destroy the entire session
-
-//session_destroy();
-
 // to delete the session 
 session_unset();
 echo "<h4>you have logged out, click here to go <a href='site.php'>Home</a></h4> ";
