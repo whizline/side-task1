fb702b44279e0b95753efcc3d600501b75758047
rogers <whizlinerogers@gmail.com>
2021-07-12 21:03:48 -0700

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start(); //to make sure its same session
-
-// to delete the session 
-session_unset();
-echo "<h4>you have logged out, click here to go <a href='site.php'>Home</a></h4> ";
-//header("Location: site.php")
-?>
